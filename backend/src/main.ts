8445f17a8dc4bf162becb5f1c8f5ba3e0448f5be
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2023-10-20 15:44:46 +0900

    🔒️ URLの設定を追加

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import { sum } from "./sum";
 
 const PORT = process.env.PORT ?? 8080;
 // フロントエンドの URL を 'http://frontend.example.com' のように指定してください。
-const CORS_ORIGIN = undefined;
+const CORS_ORIGIN = 'http://udemy-github-actions-s3-yuu.s3-website-ap-northeast-1.amazonaws.com';
 
 const app = express();
 
